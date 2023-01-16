d1bfa2fce5ee462af4adf1e944046c194526ffb4
LCDDC <122059417+LCDDC@users.noreply.github.com>
2023-01-16 14:59:34 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function cate() {
+    document.getElementById("body").style.backgroundColor="black";
+    document.getElementById("body").style.color="white";
+    document.getElementById("body").style.color="white";
+    document.getElementsByClassName("blue1").style.color="red";
+}
